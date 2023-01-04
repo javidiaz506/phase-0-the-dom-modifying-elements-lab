f3d7c5f0f848c6d17a0a61c2ee1ab9265320879f
javier <javia506@gmail.com>
2023-01-04 01:23:43 -0800

    commit tree d3cf2b10715465a135a2d1b2c64aabe059552df4

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+// Write your code
+const mainElement = document.getElementById("main");
+mainElement.parentNode.removeChild(mainElement);
+
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+const name = "YOUR-NAME";
+
+newHeader.id = "victory";
+newHeader.textContent = `${name} is the champion`;
+const parentElement = document.getElementById("parent");
+parentElement.appendChild(newHeader);
